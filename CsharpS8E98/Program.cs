173d440d7a9940ed99c9e6216899a5ada85ada83
João Lima <joao.lima@erp24.pt>
2021-03-04 09:43:10 +0000

    Projeto iniciado, mostrando ola mundo

---
+++ CsharpS8E98/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsharpS8E98
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
